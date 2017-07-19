1328355932722b24ec6975624a1a2bcf9d8a7a51
Jeremy <jmshasha@gmail.com>
2017-07-18 22:08:49 -0700

    Halfway through storyboard

---
+++ mydvds/DVDViewController.swift
@@ -0,0 +1,29 @@
+//
+//  DVDViewController.swift
+//  mydvds
+//
+//  Created by Apple on 7/18/17.
+//  Copyright © 2017 Jeremy. All rights reserved.
+//
+
+import UIKit
+
+class DVDViewController: UIViewController {
+
+  
+    @IBOutlet weak var dvdImageView: UIImageView!
+    @IBOutlet weak var titleTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func cameraTapped(_ sender: Any) {
+    }
+    @IBAction func photosTapped(_ sender: Any) {
+    }
+    @IBAction func addTapped(_ sender: Any) {
+    }
+}
