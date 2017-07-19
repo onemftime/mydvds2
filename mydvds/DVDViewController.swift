023d1546915c96b63bb083f3863d790d0cf49fee
Jeremy <jmshasha@gmail.com>
2017-07-19 15:03:22 -0700

    use this

--- mydvds/DVDViewController.swift
+++ mydvds/DVDViewController.swift
@@ -8,48 +8,22 @@
 
 import UIKit
 
-class DVDViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+class DVDViewController: UIViewController {
 
   
     @IBOutlet weak var dvdImageView: UIImageView!
     @IBOutlet weak var titleTextField: UITextField!
     
-    var imagepicker = UIImagePickerController()
-    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        imagepicker.delegate = self
+        // Do any additional setup after loading the view.
     }
 
     @IBAction func cameraTapped(_ sender: Any) {
     }
-    
     @IBAction func photosTapped(_ sender: Any) {
-        
-        imagepicker.sourceType = .photoLibrary
-        
-        present(imagepicker, animated: true, completion: nil)
-        
     }
-        
-    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
-        let image = info[UIImagePickerControllerOriginalImage] as! UIImage
-        dvdImageView.image = image
-        
-        imagepicker.dismiss(animated: true, completion: nil)
-    }
-    
-    
     @IBAction func addTapped(_ sender: Any) {
-        
-        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
-        
-        let dvd = DVD(context: context)
-        dvd.title = titleTextField.text!
-        dvd.image = UIImagePNGRepresentation(dvdImageView.image!)
-        
-        (UIApplication.shared.delegate as! AppDelegate).saveContext()
-        navigationController!.popViewController(animated: true)
     }
 }
--- mydvds/ViewController.swift
+++ mydvds/ViewController.swift
@@ -8,44 +8,18 @@
 
 import UIKit
 
-class ViewController: UIViewController,UITableViewDelegate, UITableViewDataSource {
+class ViewController: UIViewController {
 
-    @IBOutlet weak var tableView: UITableView!
-    
-    var dvds: [DVD] = []
-    
-    override func viewWillAppear(_ animated: Bool) {
-        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
-        
-        do {
-            dvds = try context.fetch(DVD.fetchRequest())
-            tableView.reloadData()
-        } catch {
-            
-        }
-        
-    }
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        tableView.delegate = self
-        tableView.dataSource = self
+        // Do any additional setup after loading the view, typically from a nib.
     }
 
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        
-        return dvds.count
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
     }
-  
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = UITableViewCell()
-        let dvd = dvds[indexPath.row]
-        cell.textLabel?.text = dvd.title!
-        cell.imageView?.image = UIImage(data: dvd.image!)
-        return cell
-    }
-    
-    
+
+
 }
 
